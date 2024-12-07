7fa325f03ebaa0f99fcfdf5ed26ce2df1033c908
Ken <tkz1996@live.com>
2024-12-07 16:56:04 +0800

    feat: added more logic

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
-
 const app = express();
+const defaultPort = 3000;
 
 app.get('/test', (request, response) => {
-    response.status(204).send('success');
+    response.status(200).send('success');
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || defaultPort;
 app.listen(port, () => console.log(`App available on http://localhost:%d`, port));
+
+function gracefulshutdown() { 
+    console.log("Shutting down"); 
+    myApp.close(() => { 
+        console.log("HTTP server closed."); 
+          
+        // When server has stopped accepting connections  
+        // exit the process with exit status 0 
+        process.exit(0);  
+    }); 
+} 
+  
+process.on("SIGTERM", gracefulshutdown);
